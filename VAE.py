615abef5fc66de73959fea5064b8ae5f2d642b0f
mark-antal-csizmadia <mark.antal.csizmadia@gmail.com>
2021-01-05 23:00:28 +0100

    Complete viz for losses added.

--- VAE.py
+++ VAE.py
@@ -100,24 +100,31 @@ class VAE(Model):
 
         """
         # Attach model parts together (i.e.: create computation graph).
+        # encoder_hidden shape=(batch_size, encoder_hidden_dim)
         encoder_hidden = self.encoder_hidden(inputs)
+        # mu shape=(batch_size, latent_dim)
         mu = self.mu(encoder_hidden)
+        # sigma shape=(batch_size, latent_dim)
         sigma = self.sigma(encoder_hidden)
+        # z shape=(batch_size, latent_dim)
         z = self.z([mu, sigma])
+        # decoder_hidden shape=(batch_size, decoder_hidden_dim)
         decoder_hidden = self.decoder_hidden(z)
+        # decoder_hidden shape=(batch_size, input_dim)
         reconstruction = self.reconstruction(decoder_hidden)
 
         # Create the loss tensors.
-        # Computes KL Divergence Loss (First part of equation 24)
-        kl_loss = kb.mean(
+        # Computes negative! KL Divergence Loss (First part of equation 24, but negative here to have -KLD)
+        negative_kl_loss = kb.mean(
             0.5 * kb.sum(-sigma + kb.exp(sigma) + kb.square(mu) - 1, axis=1))
-        # Computes teh Binary Cross-Entropy Loss (comes from the sigmoid activation function of the Bernoulli MLP layer
-        # called reconstruction).
+        # Computes the Binary Cross-Entropy Loss that is equivalent to the negative binary loglikelihood
+        # (comes from the sigmoid activation function of the Bernoulli MLP layer in the decoder)
         binary_cross_entropy_loss = kb.mean(-kb.sum(inputs * kb.log(
-            reconstruction+EPSILON) + (1-inputs) * kb.log(1-reconstruction+EPSILON), axis=1))
+            reconstruction + EPSILON) + (1 - inputs) * kb.log(1 - reconstruction + EPSILON), axis=1))
         # Create the overall VAE loss.
-        # - ELBO = KLD - Log_likelihood = BCE + KLD
-        vae_loss = binary_cross_entropy_loss + kl_loss
+        # ELBO in the paper is ELBO = KLD + Log_likelihood, ELBO in the paper is maximized
+        # We minimize the negative ELBO: - ELBO = - KLD - Log_likelihood = - KLD - Log_likelihood = - KLD + BCE
+        vae_loss = binary_cross_entropy_loss + negative_kl_loss
 
         self.add_loss(vae_loss)
 
@@ -183,23 +190,100 @@ def plot_imgs_compare(n_imgs, x, y, x_reconstructed, save_img):
     plt.show()
 
 
-def plot_lowerbound(history):
-    """ Plot likelihood lower bound
+def plot_lowerbound(history, neg_elbo_values, dataset_name, latent_dim, x_axis_label, x_axis_scale, n_data, epochs,
+                    ylim):
+    """ Plot likelihood lower bound. The loss curves of training and validation.
+
     Parameters
     ----------
     history : History
         Training and validation history.
+
+    neg_elbo_values : bool
+        If True, the ELBO values (loss values) are plotted as in the paper, that is, as negative values.
+        This is because in the paper the ELBO is being maximized. We minimize the negative ELBO, which is equivalent
+        from an optimization point of view.
+
+    dataset_name : str
+        The name of the data set. mnist for MNIST, and frey for Frey Face.
+
+    latent_dim : int
+            The number of latent dimensions into which the VAE encodes the input data.
+
+    x_axis_label : str
+        The label on the x-axis. In the paper, they show the number of samples. samples for # Training samples evaluated
+        and epochs for # Epochs evaluated.
+
+    x_axis_scale : str
+        The scale of the x-axis. In the paper they do log of number of samples. linear for linear and log for log.
+
+    n_data : int
+        The number of samples in the training data set.
+
+    epochs : int
+        The number of epochs used in the training process.
+
+    ylim : tuple
+        y_min and y_max, in this order for limit the y-axis values. In the paper, the y-axis values are limited.
+        If None, no limit is applied. Note that the limit may not show the area of interest in the plot.
+
     Returns
     -------
     None
 
     """
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('VAE Likelihood Lower Bound')
-    plt.ylabel('𝓁')
-    plt.xlabel('epoch')
-    #plt.ylim(120,220)
+    # Retrieve losses.
+    training_losses = history.history['loss']
+    validation_losses = history.history['val_loss']
+
+    # If neg_elbo_values is True, the ELBO is plotted as in the paper, that is,
+    # negative values (ELBO is being maximized).
+    if neg_elbo_values:
+        training_losses = [-training_loss for training_loss in training_losses]
+        validation_losses = [-validation_loss for validation_loss in validation_losses]
+    else:
+        pass
+
+    # Select label on x-axis and convert x-axis values accordingly.
+    if x_axis_label == "samples":
+        x_axis_label_pretty = "# Training samples evaluated"
+        x_values = [(epoch+1)*n_data for epoch in range(epochs)]
+    elif x_axis_label == "epochs":
+        x_axis_label_pretty = "# Training epochs evaluated"
+        x_values = [epoch + 1 for epoch in range(epochs)]
+    else:
+        raise Exception(f"Invalid value for x_axis_label: {x_axis_label}")
+
+    # Set x-axis scale.
+    if x_axis_scale == "linear":
+        x_axis_scale_pretty = "linear"
+    elif x_axis_scale == "log":
+        x_axis_scale_pretty = "log"
+    else:
+        raise Exception(f"Invalid x_axis_scale: {x_axis_scale}")
+
+    # Check dataset_name.
+    if dataset_name == "mnist":
+        dataset_name_pretty = "MNIST"
+    elif dataset_name == "frey":
+        dataset_name_pretty = "Frey Face"
+    else:
+        raise Exception(f"Invalid dataset_name: {dataset_name}")
+
+    # Plot training and validation losses.
+    plt.plot(x_values, training_losses, c="r")
+    plt.plot(x_values, validation_losses, c="b")
+    # Set x-axis scale.
+    plt.xscale(x_axis_scale_pretty)
+    # Set axis labels.
+    plt.xlabel(x_axis_label_pretty)
+    plt.ylabel(r"$\mathcal{L}$", rotation=90)
+    # Set figure title.
+    fig_title = dataset_name_pretty + r", $N_{\mathbf{z}}$ = " + str(latent_dim)
+    plt.title(fig_title)
+
+    if ylim is not None:
+        plt.ylim(ylim[0], ylim[1])
 
     plt.legend(['train', 'val'], loc='upper left')
     plt.savefig("images/lower_bound.png")
@@ -223,33 +307,32 @@ def freyface():
 
     return train,test,height,width
 
+
 if __name__ == "__main__":
     # Uncomment for freyface
     
-    x_train_flattened,x_test_flattened,ff_height,ff_width = freyface()
+    """x_train_flattened,x_test_flattened,ff_height,ff_width = freyface()
     n_data, height, width = len(x_train_flattened),ff_height,ff_width
-    input_dim = ff_height * ff_width
+    input_dim = ff_height * ff_width"""
     
     # Uncomment for mnist
-    '''
     # Import the data set.
-    #mnist = tf.keras.datasets.mnist
-    #(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    #n_data, height, width = x_train.shape
+    n_data, height, width = x_train.shape
 
     input_dim = height * width
     # Reshape data to (60000,784) in mnist case
-    #x_train_flattened = x_train.reshape(-1, input_dim).astype("float32") / 255
-    #x_test_flattened = x_test.reshape(-1, input_dim).astype("float32") / 255
-    '''
+    x_train_flattened = x_train.reshape(-1, input_dim).astype("float32") / 255
+    x_test_flattened = x_test.reshape(-1, input_dim).astype("float32") / 255
 
     #x_train = x_train[np.random.randint(x_train.shape[0],size=1000)]
 
 
     # Create the VAE.
     encoder_hidden_dim = 100
-    latent_dim = 3
+    latent_dim = 20
     decoder_hidden_dim = 100
     vae = VAE(input_dim=input_dim, encoder_hidden_dim=encoder_hidden_dim, latent_dim=latent_dim,
               decoder_hidden_dim=decoder_hidden_dim, name="vae")
@@ -267,7 +350,23 @@ if __name__ == "__main__":
     batch_size = 32
     history = vae.fit(x_train_flattened, x_train_flattened,
                       epochs=epochs, batch_size=batch_size, shuffle=True, validation_data=(x_test_flattened, x_test_flattened))
-    plot_lowerbound(history)
+
+    neg_elbo_values = True
+    dataset_name = "mnist"
+    x_axis_label = "samples"
+    x_axis_scale = "log"
+    ylim = (-150, -100)
+
+    plot_lowerbound(
+        history=history,
+        neg_elbo_values=neg_elbo_values,
+        dataset_name=dataset_name,
+        latent_dim=latent_dim,
+        x_axis_label=x_axis_label,
+        x_axis_scale=x_axis_scale,
+        n_data=n_data,
+        epochs=epochs,
+        ylim=ylim)
 
     # Reconstruct training data.
     x_train_reconstructed_flattened = vae.predict(x_train_flattened)
@@ -275,8 +374,8 @@ if __name__ == "__main__":
         n_data, height, width)
 
     # Visualize the reconstructed images.
-    #plot_imgs_compare(n_imgs=10, x=x_train, y=y_train,
-    #                  x_reconstructed=x_train_reconstructed, save_img=True)
+    plot_imgs_compare(n_imgs=10, x=x_train, y=y_train,
+                      x_reconstructed=x_train_reconstructed, save_img=True)
 
 
     """
